7fece3abe3f4909a9fc4a4bab844bb9f26bb9180
nikolay_prasolov <docum.magic0@gmail.com>
2026-01-09 18:56:50 +0300

    url validation

--- src/modules/blogs/api/input-dto/blogs.input-dto.ts
+++ src/modules/blogs/api/input-dto/blogs.input-dto.ts
@@ -1,5 +1,5 @@
 //dto для боди при создании юзера. Сюда могут быть добавлены декораторы swagger
-import { IsString } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
 export class CreateBlogInputDto {
   @IsString({ message: 'name must be a string' })
@@ -7,5 +7,8 @@ export class CreateBlogInputDto {
   @IsString({ message: 'description must be a string' })
   description: string;
   @IsString({ message: 'websiteUrl must be a string' })
+  @Matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/, {
+    message: 'websiteUrl must match the required pattern',
+  })
   websiteUrl: string;
 }
