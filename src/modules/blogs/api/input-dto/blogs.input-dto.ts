1dbb08c45268a7b9e36e78d878cbce90ac542c2e
nikolay_prasolov <docum.magic0@gmail.com>
2026-01-09 16:29:46 +0300

    fix

--- src/modules/blogs/api/input-dto/blogs.input-dto.ts
+++ src/modules/blogs/api/input-dto/blogs.input-dto.ts
@@ -1,6 +1,11 @@
 //dto для боди при создании юзера. Сюда могут быть добавлены декораторы swagger
+import { IsString, isString } from 'class-validator';
+
 export class CreateBlogInputDto {
+  @IsString({ message: 'name must be a string' })
   name: string;
+  @IsString({ message: 'description must be a string' })
   description: string;
+  @IsString({ message: 'websiteUrl must be a string' })
   websiteUrl: string;
 }
