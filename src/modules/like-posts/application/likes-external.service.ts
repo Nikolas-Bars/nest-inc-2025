94af3ef68643970da41ba6187c59a4b0861e2257
nikolay_prasolov <docum.magic0@gmail.com>
2026-01-17 08:52:06 +0300

    like post repo

--- src/modules/like-posts/application/likes-external.service.ts
+++ src/modules/like-posts/application/likes-external.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LikesExternalService {
+  // constructor(private readonly ) {}
   async updatePostLikeStatus(postId: string): Promise<boolean> {
     return false
   }
---
+++ src/modules/like-posts/infrastructure/like-post.repository.ts
@@ -0,0 +1,39 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { LikePost, LikePostDocument } from '../domain/like-post.entity';
+import type { LikePostModelType } from '../domain/like-post.entity';
+import { LikePostDomainDto } from '../domain/dto/like-post.domain.dto';
+
+@Injectable()
+export class LikePostRepository {
+  //инжектирование модели через DI
+  constructor(
+    @InjectModel(LikePost.name) private LikePostModel: LikePostModelType,
+  ) {}
+
+  create(dto: LikePostDomainDto): LikePostDocument {
+    return this.LikePostModel.createInstance(dto);
+  }
+
+  async findById(id: string): Promise<LikePostDocument | null> {
+    return this.LikePostModel.findOne({
+      _id: id,
+      deletedAt: null,
+    });
+  }
+
+  async save(likePost: LikePostDocument) {
+    await likePost.save();
+  }
+
+  async findOrNotFoundFail(id: string): Promise<LikePostDocument> {
+    const likePost = await this.findById(id);
+
+    if (!likePost) {
+      //TODO: replace with domain exception
+      throw new NotFoundException('like status not found');
+    }
+
+    return likePost;
+  }
+}
